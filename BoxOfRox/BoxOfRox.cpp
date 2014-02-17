a40a9a49a9312c60e64ad21289cf65d1ed6ad1a7
moonguy <viranga@asu.edu>
2014-02-17 14:45:05 -0800

    Tried to add a number of self-gravitating grains into the box.

--- BoxOfRox/BoxOfRox.cpp
+++ BoxOfRox/BoxOfRox.cpp
@@ -54,6 +54,8 @@ bool ConfigExperimentOptions()
 	ncc::GetStrPropertyFromINIFile("experiment","experiment_type","",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
 	if		(strcmp(buf,"fill_box")==0)
 		rox::eExperimentType = rox::eFILL_BOX;
+	else if (strcmp(buf,"shake_box")==0)
+		rox::eExperimentType = rox::eSHAKE_BOX;
 	else
 		rox::eExperimentType = rox::eBAD_EXPERIMENT_TYPE;
 	
@@ -62,17 +64,37 @@ bool ConfigExperimentOptions()
 	rox::params.boxSize = atof(buf);
 
 	// Parameters of the grain size distribution
-	ncc::GetStrPropertyFromINIFile("experiment","gsd_type","uniform",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	ncc::GetStrPropertyFromINIFile("experiment","grain_type","uniform",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
 	if		(strcmp(buf,"uniform")==0)
-		rox::gsd.type = rox::gsd.eGSD_UNIFORM;
+		rox::grain.type = rox::grain.eGRAIN_UNIFORM;
+	else if	(strcmp(buf,"bimodal")==0)
+		rox::grain.type = rox::grain.eGRAIN_BIMODAL;
 	else
-		rox::gsd.type = rox::gsd.eBAD_GSD_TYPE;
+		rox::grain.type = rox::grain.eBAD_GRAIN_TYPE;
+
+	ncc::GetStrPropertyFromINIFile("experiment","grain_size1","1",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	rox::grain.size1 = atof(buf);
+	ncc::GetStrPropertyFromINIFile("experiment","grain_size2","1",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	rox::grain.size2 = atof(buf);
+
+	rox::grain.totalNumber = ncc::GetIntPropertyFromINIFile("experiment","grain_total_number",0,gRun.iniFile.c_str());
+	
+	ncc::GetStrPropertyFromINIFile("experiment","nucleus_radius", "0",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	rox::params.nucleusRadius = atof(buf);
+
+	// Parameters for box agitation
+	ncc::GetStrPropertyFromINIFile("experiment","shake_magnitude","0",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	rox::params.shakeMagnitude = atof(buf);
+	
+	// Code units and scaling
+	ncc::GetStrPropertyFromINIFile("units","big_g","0",buf,MAX_CHARS_PER_NAME,gRun.iniFile.c_str());
+	rox::units.bigG = atof(buf);
 
 	return success;
 }
 void CustomizeScene(PxSceneDesc &sceneDesc)
 {
-	
+	sceneDesc.gravity = PxVec3(0);
 }
 void CustomizeGLUT()
 {
@@ -99,7 +121,7 @@ void PrintDebug()
 }
 void ApplyCustomInteractions()
 {
-	
+	rox::GravitateSelf();
 }
 void RenderOtherStuff()
 {
@@ -108,7 +130,6 @@ void RenderOtherStuff()
 void CreateExperiment()
 {
 	gDebug.bXYGridOn=true;
-	
 	switch (rox::eExperimentType)
 	{
 	case rox::eFILL_BOX:
@@ -118,6 +139,11 @@ void CreateExperiment()
 	default:
 		ncc__error("Unknown experiment type. Experiment aborted.\a");
 	}
+
+	// Move the camera to where you can see TODO: replace with a function call
+	FindExtremers();
+	if (gExp.VIPs.extremers.outmost)
+		gCamera.pos.z = gExp.VIPs.extremers.outmost->getGlobalPose().p.z + 10*gExp.defGrainSize;
 
 	// Start the action
 	gSim.isRunning=true;
@@ -185,10 +211,139 @@ void rox::CreateContainment()
 	gPhysX.mScene->addActor(*theBox);
 	rox::VIPs.theBox = theBox;
 }
-
 void rox::CreateFillBoxExperiment()
 {
 	rox::CreateContainment();
+
+	/*
+ * This rubble-pile creation method places grains in a volume ????
+
+ * In the "uniform" variant, all grains share a convex mesh, just scaled differently.
+ * TODO: In the non-uniform variant, grains are generated individually, with a size scale.
+ * TODO: Implement more size distributions (currently bimodal)
+ * TODOL Implement more nucleus options (currently capsule)
+*/
+
+	// place the nucleus
+	if (rox::params.nucleusRadius)
+		rox::VIPs.nucleus = CreateRubbleGrain(PxVec3(0),eCAPSULE_GRAIN,rox::params.nucleusRadius/2,*gPhysX.mDefaultMaterial);
+	if (rox::VIPs.nucleus)
+	{
+		ColorActor(rox::VIPs.nucleus,ncc::rgb::rDarkRed);
+		rox::VIPs.nucleus->setRigidDynamicFlag(PxRigidDynamicFlag::eKINEMATIC,true);
+	}
+
+	// Place actors
+	for (PxU32 k=0; k<positions.size(); k++)
+	{
+		// select a size for the next grain
+		PxReal grainScale = rox::grain.size1;
+		//bool isSize2 = (k % (rox::grain.numberRatio)) == 0;
+		bool isSize2 = (rox::grain.type==rox::grain.eGRAIN_BIMODAL && k < rox::grain.numberRatio);
+		if (rox::grain.type==rox::grain.eGSD_BIMODAL && isSize2)
+			grainScale = rox::grain.size2;
+		
+		// create a convex geometry for the next grain
+		PxMeshScale meshScale;
+		meshScale.scale = PxVec3(grainScale);
+		PxMat33 M = meshScale.toMat33();
+		PxConvexMeshGeometry meshGeometry(theMesh,meshScale);
+
+		// create a convex actor from this geometry
+		PxRigidDynamic* aGrain = PxCreateDynamic(*gPhysX.mPhysics,PxTransform(positions[k]),meshGeometry,*gPhysX.mDefaultMaterial,gExp.defGrainDensity);
+		if (aGrain)
+		{
+			if (gPhysX.props.sleepThreshold > -1) aGrain->setSleepThreshold(0.5*gPhysX.props.sleepThreshold*gPhysX.props.sleepThreshold);
+			if (gPhysX.props.angularDamping > -1) aGrain->setAngularDamping(gPhysX.props.angularDamping);
+			if (gPhysX.props.linearDamping  > -1) aGrain->setLinearDamping(gPhysX.props.linearDamping);
+
+			if (isSize2)
+			{
+				aGrain->setName("size2");
+				ColorActor(aGrain,ncc::rgb::oDarkOrange);
+			}
+			else
+			{
+				aGrain->setName("size1");
+			}
+
+			RandOrientActor(aGrain);
+
+			gPhysX.mScene->addActor(*aGrain);
+		}
+	}
+}
+void rox::GravitateSelf()
+{
+	if (gCUDA.cudaCapable)
+		rox::GravitateOnDevice();
+	else
+		rox::GravitateOnHost();
+}
+void rox::GravitateOnHost()
+/*
+ * This is a semi-optimized all-pairs gravity calculation in a single host thread. Not
+ * usually the best option, but a necessary fall back option.
+*/
+{
+	// Prepare
+	PxU32 nbActors = gPhysX.mScene->getActors(gPhysX.roles.dynamics,gPhysX.cast,MAX_ACTORS_PER_SCENE);
+	if (nbActors<2) return;
+	float *bodies = new float[4*nbActors];
+	float *forces = new float[3*nbActors];
+	if (bodies==NULL || forces==NULL)
+		ncc__error("Error allocating memory for body/forces arrays.");
+
+	// Linearize actor positions
+	for (PxU32 k=0; k<nbActors; k++)
+	{
+		PxRigidDynamic* actor = gPhysX.cast[k]->isRigidDynamic();
+		PxTransform pose = actor->getGlobalPose().transform(actor->getCMassLocalPose());
+		PxVec3 pos = pose.p;
+		bodies[4*k+0] = actor->getMass();
+		bodies[4*k+1] = pos.x;
+		bodies[4*k+2] = pos.y;
+		bodies[4*k+3] = pos.z;
+	}
+
+	// And now, the N^2 double loop.
+	for (PxU32 j=0; j<nbActors; j++)
+	{
+		forces[3*j+0] = forces[3*j+1] = forces[3*j+2] = 0.0f;
+		for (PxU32 k=0; k<j; k++)
+		{
+			PxReal x = bodies[4*k+1] - bodies[4*j+1];
+			PxReal y = bodies[4*k+2] - bodies[4*j+2];
+			PxReal z = bodies[4*k+3] - bodies[4*j+3];
+
+			float distSqr = x*x + y*y + z*z;
+			float distSix = distSqr*distSqr*distSqr;
+			float invDistCube = 1.0f/sqrtf(distSix);
+
+			float s = rox::units.bigG * bodies[4*j+0] * bodies[4*k+0] * invDistCube;
+
+			forces[3*j+0] += x*s;
+			forces[3*j+1] += y*s;
+			forces[3*j+2] += z*s;
+			forces[3*k+0] -= x*s;
+			forces[3*k+1] -= y*s;
+			forces[3*k+2] -= z*s;
+		}
+	}
+
+	// Add accumulated forces to actors
+	for (PxU32 k=0; k<nbActors; k++)
+	{
+		PxRigidDynamic* actor = gPhysX.cast[k]->isRigidDynamic();
+		PxVec3 F(forces[3*k+0],forces[3*k+1],forces[3*k+2]);
+		actor->addForce(F);
+	}
+
+	// Clean up
+	delete [] bodies;
+	delete [] forces;
+
+	return;
 }
 
 // End lint level warnings
