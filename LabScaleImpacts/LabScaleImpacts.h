c1a920ad3059a11c20a1393cddaf29b1992ade6e
Naor Movshovitz <nmovshov@gmail.com>
2016-01-02 15:10:40 -0800

    cloning new project LabScaleImpacts based on Verify. Looks okay so far. Basically just made a copy of the old_proj directory, renamed it new_proj, and edited the .proj files just for names and added to same solution.

---
+++ LabScaleImpacts/LabScaleImpacts.h
@@ -0,0 +1,82 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Header file for project Verify. This project implements a suite of tests of rigid body
+// dynamics in PhysX. Several scenarios can be simulated, that have either an analytic
+// solution or at least a conserved quantity that can be measured. The available tests
+// are:
+//
+// * Slider - a box on an inclined plane, testing the friction model in PhysX
+// 
+// * Collider - a collision between shapes of varying complexity, testing the collision
+//              detection and collision resolution in PhysX.
+//
+// * Ball on ground - test of internal gravity in PhysX
+//
+// * Ball on ball - test of integration of manually implemented gravity
+//
+// Author: Naor Movshovits (nmovshov at google dot com)
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef VERIFY_H
+#define VERIFY_H
+namespace verify
+{
+enum {eBAD_EXPERIMENT_TYPE, eCOLLIDER, eINCLINER, eTUMBLER, eTUMBLERS, eBALL_ON_GROUND,
+      eSPRINGER_EXPERIMENT} eExperimentType;
+
+PxReal spinMag; // Spin magnitude for target in collider experiment
+PxReal kickMag; // Kick magnitude for bullet in collider experiment
+
+PxTransform R1, R0; // throwaway transform holders with static duration
+PxVec3 V;           // throwaway vector with static duration
+
+struct {
+	unsigned int actorDiag;
+	unsigned int systemDiag1;
+	unsigned int systemDiag2;
+	unsigned int systemDiag3;
+} hudMsgs;
+
+struct {
+	PxRigidDynamic* bullet;
+	PxRigidDynamic* target;
+	PxRigidDynamic* tumbler;
+	PxRigidDynamic* ball1;
+	PxRigidDynamic* ball2;
+	vector <PxRigidDynamic*> Tumblers;
+	PxRigidDynamic* incliner;
+} VIPs;
+
+struct {
+	PxVec3 w;
+	PxVec3 L_now, L_true;
+	PxReal K_now, K_true;
+	PxRigidDynamic* handle;
+} tumbler;
+
+struct {
+	int inc;
+	PxRigidDynamic* handle;
+} incliner;
+
+struct {
+	PxReal littleG;
+	PxReal bigG;
+	PxReal springK;
+} units;
+
+void CreateColliderExperiment();
+void CreateTumblerExperiment();
+void CreateTumblersExperiment();
+void CreateInclinerExperiment();
+void CreateBallOnGroundExperiment();
+void CreateSpringerExperiment();
+void CalcTumblerDynamics();
+void InclineGravity(PxReal deg);
+void LogTumblerExperiment();
+void LogBallOnGroundExperiment();
+void LogSpringerExperiment();
+void ControlBallOnGroundExperiment();
+void ControlSpringerExperiment();
+};
+
+#endif
---
+++ LabScaleImpacts/readme.txt
@@ -0,0 +1,19 @@
+VERIFY - A project with several tests of simple rigid body dynamics
+
+The tests currently implemented are (use these names in the experiment_type field of verify.ini):
+
+tumbler - a freely rotating solid body of various shapes. A test of time integration and general dynamics.
+
+collider - a rotating target shape and a moving bullet shape colliding. A test of collision detection and resolution.
+
+incliner - a box on an inclined plane. A test of the friction model. Use up/down arrows to incline/decline the plane (actually the gravity vector).
+
+ball_on_ground - a ball of radius 1 dropped from height 6 to test integration accuracy.
+
+Controls:
+WASD to move
+QZ to up/down
+Left mouse to rotate
+F10 to reset
+p to pause/unpause
+ESC to quit
