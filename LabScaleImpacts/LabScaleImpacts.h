c335d947a4831bfa3e6edbd5b2b62dc75cb7f5cc
Naor Movshovitz <nmovshov@gmail.com>
2016-01-14 11:45:11 -0800

    indenting LabScaleImpacts.h restarting work on SGP

--- LabScaleImpacts/LabScaleImpacts.h
+++ LabScaleImpacts/LabScaleImpacts.h
@@ -3,79 +3,80 @@
 // velocity impacts into regolith in uniform 1g gravity. Idea is of course to
 // benchmark against lab tests.
 //
-// Author: Naor Movshovits (nmovshov at google dot com)
+// Author: Naor Movshovitz (nmovshov at google dot com)
 /////////////////////////////////////////////////////////////////////////////////
 
 #ifndef LABSCALE_H
 #define LABSCALE_H
 namespace labscale
 {
-// Implement different scenarios with different experiment labels. The dispatcher
-// function CreateExperiment will call different routines, basically totally
-// different programs that share mostly just the dispatching and logging mechanism.
-enum {eBAD_EXPERIMENT_TYPE, eHOLSAPPLE1} eExperimentType;
-enum {eBAD_EXP_SUBTYPE, eFILL_BOX, eTILT_BOX, ePENETRATOR} eExperimentSubtype;
+	// Implement different scenarios with different experiment labels. The dispatcher
+	// function CreateExperiment will call different routines, basically totally
+	// different programs that share mostly just the dispatching and logging mechanism.
+	enum {eBAD_EXPERIMENT_TYPE, eHOLSAPPLE1} eExperimentType;
+	enum {eBAD_EXP_SUBTYPE, eFILL_BOX, eTILT_BOX, ePENETRATOR} eExperimentSubtype;
 
-// Some throwaway objects with static duration
-PxTransform R1, R0; // throwaway transform holders
-PxVec3 V;           // throwaway vector
+	// Some throwaway objects with static duration
+	PxTransform R1, R0; // throwaway transform holders
+	PxVec3 V;           // throwaway vector
 
-// Really dumb way to implement hud messages; but it works
-struct {
-    unsigned int actorDiag;
-    unsigned int systemDiag1;
-    unsigned int systemDiag2;
-    unsigned int systemDiag3;
-} hudMsgs;
+	// Really dumb way to implement hud messages; but it works
+	struct {
+		unsigned int actorDiag;
+		unsigned int systemDiag1;
+		unsigned int systemDiag2;
+		unsigned int systemDiag3;
+	} hudMsgs;
 
-// Named actors
-struct {
-    PxRigidDynamic* container;
-    PxRigidDynamic* ball1;
-} VIPs;
+	// Named actors
+	struct {
+		PxRigidDynamic* container;
+		PxRigidDynamic* ball1;
+	} VIPs;
 
-// A regolith container
-struct {
-    PxReal diameter;
-    PxReal fillHeight;
-	PxReal pourRate;
-	PxReal tiltRate;
-	bool bTilt;
-} reg_box;
+	// A regolith container
+	struct {
+		PxReal diameter;
+		PxReal fillHeight;
+		PxReal pourRate;
+		PxReal tiltRate;
+		bool bTilt;
+	} reg_box;
 
-// And regolith
-struct {
-    PxReal diameter;
-    int nbGrains;
-    PxMaterial *material;
-    RubbleGrainType grainType;
-    PxReal materialDensity;
-} regolith;
-struct {
-    PxReal diameter;
-    PxReal materialDensity;
-    PxReal speed;
-    RubbleGrainType grainType;
-    PxReal iniSurface;
-} impactor;
+	// And regolith
+	struct {
+		PxReal diameter;
+		int nbGrains;
+		PxMaterial *material;
+		RubbleGrainType grainType;
+		PxReal materialDensity;
+	} regolith;
+	struct {
+		PxReal diameter;
+		PxReal materialDensity;
+		PxReal speed;
+		RubbleGrainType grainType;
+		PxReal iniSurface;
+	} impactor;
 
-// Measured and diagnostic quantities
+	// Measured and diagnostic quantities
 
-// Units
-struct {
-    PxReal littleG;
-} units;
+	// Units
+	struct {
+		PxReal littleG;
+	} units;
 
-void CreatePenetratorExperiment();
-void CreateFillBoxExperiment();
-void CreateTiltBoxExperiment();
-void LogPenetratorExperiment();
-void ControlPenetratorExperiment();
-void ControlFillBoxExperiment();
-void ControlTiltBoxExperiment();
-void CreateRegolithContainer();
-PxReal getRegolithSurface();
-PxRigidDynamic *CreateRegolithGrain();
+	// Namespace functions
+	void CreatePenetratorExperiment();
+	void CreateFillBoxExperiment();
+	void CreateTiltBoxExperiment();
+	void LogPenetratorExperiment();
+	void ControlPenetratorExperiment();
+	void ControlFillBoxExperiment();
+	void ControlTiltBoxExperiment();
+	void CreateRegolithContainer();
+	PxReal getRegolithSurface();
+	PxRigidDynamic *CreateRegolithGrain();
 };
 
 #endif
--- SGP/SGP.h
+++ SGP/SGP.h
@@ -1,17 +1,25 @@
-///////////////////////////////////////////////////////////////////////////////
-// Header file for project SGP. Stands for Self-Gravitating Pile. This project
-// is normally used to load a rubble pile from some source, impose some initial
+/////////////////////////////////////////////////////////////////////////////////
+// Header file for project SGP. Stands for Self-Gravitating Pile. This project is
+// normally used to load a rubble pile from some source, impose some initial
 // conditions, and then let it evolve.
 //
-// Author: Me (Naor)
-///////////////////////////////////////////////////////////////////////////////
+// Author: Naor Movshovitz (nmovshov at google dot com)
+/////////////////////////////////////////////////////////////////////////////////
 
 #ifndef SGP_H
 #define SGP_H
 namespace sgp
 {
+	// Implement different scenarios with different experiment labels. The dispatcher
+	// function CreateExperiment will call different routines, basically totally
+	// different programs that share mostly just the dispatching and logging mechanism.
 	enum {eBAD_EXPERIMENT_TYPE, eMAKE_SGP, eLOAD_SGP, eTEST_SCALING} eExperimentType;
 
+	// Some throwaway objects with static duration
+	PxTransform R1, R0; // throwaway transform holders
+	PxVec3 V;           // throwaway vector
+
+	// Really dumb way to implement hud messages; but it works
 	struct {
 		unsigned int systemDiag1;
 		unsigned int systemDiag2;
@@ -19,6 +27,13 @@ namespace sgp
 		unsigned int systemDiag4;
 		unsigned int systemDiag5;
 	} hudMsgs;
+
+	// Named actors
+	struct {
+		PxRigidDynamic* nucleus;
+		PxRigidDynamic* lBall;
+		PxRigidDynamic* rBall;
+	} VIPs;
 
 	struct {
 		PxReal nucleusRadius;
@@ -36,19 +51,12 @@ namespace sgp
 		PxU32  totalNumber;
 	} gsd; // grain size distribution
 
+	// Units
 	struct {
-		PxRigidDynamic* nucleus;
-		PxRigidDynamic* lBall;
-		PxRigidDynamic* rBall;
-	} VIPs;
-
-	struct {
-		PxReal length;
-		PxReal mass;
-		PxReal velocity;
 		PxReal bigG;
 	} units;
 
+	// Namespace functions
 	void CreateMakeSGPExperiment();
 	void CreateLoadSGPExperiment();
 	void CreateTestScalingExperiment();
