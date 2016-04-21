b419a9fbed89fcaf9f025a33fb405d795706ce99
Naor Movshovitz <nmovshov@gmail.com>
2016-04-20 19:14:08 -0700

    interim commit

---
+++ SGP/helpers/CheckSGPOrbit.m
@@ -0,0 +1,35 @@
+%% Check SGP orbit accuracy
+clear
+close all
+clc
+filename = 'test.orb';
+
+%% Load orbit log
+try
+    raw = importdata(filename,' ',headcount(filename));
+catch
+    [filename, pathname] = uigetfile('*.out');
+    filename = fullfile(pathname, filename);
+    raw = importdata(filename,' ',headcount(filename));
+end
+disp(raw.textdata);
+t = raw.data(:,1);
+x = raw.data(:,2);
+y = raw.data(:,3);
+
+%% Calculate some measured diagnostics
+r = hypot(x,y);
+[q, ind] = min(r);
+q_p = [x(ind), y(ind)];
+[Q, ind] = max(r);
+Q_p = [x(ind), y(ind)];
+ecc = (Q - q)/(Q + q);
+
+%% Plot orbit in lab frame
+plot(x,y)
+hold
+plot(q_p(1), q_p(2), 'r+')
+plot(0,0,'ko');
+axis equal
+%figure
+%plot(t,r)
--- SGP/helpers/orbits.m
+++ SGP/helpers/orbits.m
@@ -14,67 +14,74 @@ orbit_type = 'bound'; % ['bound'|'hyperbolic']
 bigM = 2e6;
 bigG = 6.674e-5;
 q = 20;
-e = 0.2; % bound only
+e = 0.4; % bound only
 vinf = 0.1; % hyperbolic only
-r_ini = 2*q;
-r_end = 2*q;
+r_ini = 1; % for bound this is a fraction of Q (for full orbit use 1) and ...
+r_end = 0.9; % ... for hyperbolic this is a multiple of q
 dt = 0.02;
 
-%% The time span (t=0 at peri-)
-% start by calculating the derived orbital elements
-a=q/(1-e); % semi-major axis
-Q=a*(1+e); % ap- (just for input checking)
-if r_ini<q || r_end<q || r_ini>Q || r_end>Q
-    error('don''t be a dick')
+switch orbit_type
+    case 'bound'
+        % Start by calculating the derived orbital elements
+        a = q/(1 - e); % semi-major axis
+        Q = a*(1 + e); % ap- (just for input checking)
+        r_ini = r_ini*Q;
+        r_end = r_end*Q;
+        if (r_ini < q) || (r_end < q) || (r_ini > Q) || (r_end > Q)
+            error('don''t be a dick')
+        end
+        P = 2*pi*sqrt(a^3/(bigG*bigM)); % orbital period
+        
+        % Calculate initial and final eccentric anomalies
+        cosE_ini = (1 - r_ini/a)/e;
+        E_ini = -acos(cosE_ini); % pre-peri- negative angle
+        if e == 0, E_ini = -pi*r_ini/Q; end
+        cosE_end=(1-r_end/a)/e;
+        E_end=acos(cosE_end); % post-per- positive angle
+        if e == 0, E_end = pi*r_ini/Q; end
+        if ~(isreal(E_ini) && isreal(E_end))
+            error('something''s wrong')
+        end
+        
+        % Calculate initial and final times corresponding to these "angles"
+        t_ini = P/(2*pi)*(E_ini - e*sin(E_ini));
+        t_end = P/(2*pi)*(E_end - e*sin(E_end));
+        
+        % And create a time vector
+        tVec = t_ini:dt:t_end;
+        
+        % Invert Kepler's equation
+        % Shunning premature optimization I will try the naive approach first,
+        % numerically converging on each time point independently. To not be
+        % comepletely dense I will at least use the previously found time point
+        % as a starting guess.
+        kepler = @(tau,psi)tau-P/(2*pi)*(psi-e*sin(psi)); % Kepler's equation
+        EVec = ones(size(tVec));
+        EVec(1) = double(E_ini); % We know the first point
+        for k=2:numel(tVec)
+            fun = @(psi)double(kepler(tVec(k),psi));
+            guess = EVec(k-1);
+            EVec(k)=fzero(fun,guess);
+        end
+        
+        % Translate eccentric anomalies back to meaningful space coordinates
+        fVec = acos((cos(EVec) - e)./(1 - e*cos(EVec))); % orbit phase angle
+        fVec = fVec.*sign(EVec); % make pre-peri angles negative
+        rVec = a*(1 - e^2)./(1 + e*cos(fVec)); % orbit equation
+        xVec = rVec.*cos(fVec);
+        yVec = rVec.*sin(fVec);
+    case 'hyperbolic'
+        disp 'coming soon'
+    case default
+        error('Unknown orbit type')
 end
-P=2*pi*sqrt(a^3/(bigG*bigM)); % orbital period
-% then get the initial and final eccentric anomalies from the specified
-% initial and final distances
-cosE_ini=(1-r_ini/a)/e;
-E_ini=-acos(cosE_ini); % pre-peri- negative angle
-cosE_end=(1-r_end/a)/e;
-E_end=acos(cosE_end); % post-per- positive angle
-if ~(isreal(E_ini)&&isreal(E_end))
-    error('something''s wrong')
-end
-% then get the initial and final times corresponding to these angles
-% (remember that t=0 is at peri- so the initial time will be negative)
-t_ini=P/(2*pi)*(E_ini-e*sin(E_ini));
-t_end=P/(2*pi)*(E_end-e*sin(E_end));
-% and create a time vector
-tVec=t_ini:dt:t_end;
 
-%% The hard part - inverting Kepler's equation
-% Shunning premature optimization i will try the naive approach first.
-% Numerically converging on each time point independently. To not be
-% comepletely obtuse, I will at least use the previously found time point
-% as a starting guess...
-kepler=@(tau,psi)tau-P/(2*pi)*(psi-e*sin(psi)); % Kepler's equation as fun
-EVec=ones(size(tVec));
-EVec(1)=double(E_ini); % we know the first point.
-for k=2:numel(tVec)
-    fun=@(psi)double(kepler(tVec(k),psi));
-    guess=EVec(k-1);
-    EVec(k)=fzero(fun,guess);
-end
-
-%% Translate eccentric anomalies to meaningful space coordinates
-fVec=acos((cos(EVec)-e)./(1-e*cos(EVec))); % orbit phase angle
-fVec=fVec.*sign(EVec); % make pre-peri angles negative
-rVec=a*(1-e^2)./(1+e*cos(fVec)); % orbit equation
-xVec=rVec.*cos(fVec);
-yVec=rVec.*sin(fVec);
-
-%% Save orbit data to file
-% a .orb file with header will be created (and overrun) in the current
-% directory
-fid=fopen([orbit_name '.orb'],'wt');
-fprintf(fid,'%s\n%s\n%s\n%s\n',...
-    '---BEGIN HEADER---',...
-    'Orbital data in cartesian coordinates (most likely mks).',...
-    'Columns are:',...
-    '[t] [x] [y]',...
-    '---END HEADER---');
+%% Save orbit to file
+% File with header will be created (and overrun) in the current directory
+fid = fopen([orbit_name '.orb'],'wt');
+fprintf(fid,'# Orbital (Cartesian) coordinates in code units.\n');
+fprintf(fid,'# Columns are:\n');
+fprintf(fid,'# [t]  [x]  [y]\n\n');
 fclose(fid);
-orbit=double([tVec' xVec' yVec']);
+orbit = double([tVec' xVec' yVec']);
 save([orbit_name '.orb'],'orbit','-ascii','-append')
